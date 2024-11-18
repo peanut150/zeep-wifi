bbb8481f33683371a8fcda0f306ce7f1ec413e5f
anna <anniegabriento51@gmail.com>
2024-11-18 14:57:02 +0800

    Chore (#003): remove unused imports

--- src/main/java/com/example/zeepwifi/models/JwtClaim.java
+++ src/main/java/com/example/zeepwifi/models/JwtClaim.java
@@ -2,8 +2,6 @@ package com.example.zeepwifi.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import lombok.Data;
-
 @JsonIgnoreProperties
 public class JwtClaim {
     public String accountUsername;
